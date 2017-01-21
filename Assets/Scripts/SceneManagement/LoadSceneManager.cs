845464a81e4fd4204b40cf25b4eec450199320f1
oms064 <otokonoko064@gmail.com>
2017-01-21 03:18:09 -0600

    Se agregaron nuevas escenas así como el LoadSceneManager que se encargará de administrarlas. MainMenu ya tiene un botón de prueba para pasar a la escena Game.

---
+++ Assets/Scripts/SceneManagement/LoadSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour {
+    [HideInInspector]
+    public Scenes currentScene = Scenes.MAIN_MENU;
+    [HideInInspector]
+    public static LoadSceneManager instance;
+
+    public Scenes nextScene;
+
+    public void LoadNextScene() {
+        SceneManager.LoadScene((int)nextScene);
+        currentScene = nextScene;
+    }
+
+    public void ReloadScene() {
+        SceneManager.LoadScene((int)currentScene);
+    }
+}
